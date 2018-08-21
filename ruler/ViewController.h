93818a41c9c9b41f31a47df85657a9b1a2b751b1
yk123 <1123250596@qq.com>
2018-08-21 11:28:02 +0800

    Initial Commit

---
+++ ruler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ruler
+//
+//  Created by 严凯 on 2018/8/21.
+//  Copyright © 2018年 严凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ruler/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ruler
+//
+//  Created by 严凯 on 2018/8/21.
+//  Copyright © 2018年 严凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
